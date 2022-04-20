2c989a6a90e658a5b7d13c17881ed982e2f482d8
naveenkumar7352 <100106641+naveenkumar7352@users.noreply.github.com>
2022-04-20 21:30:22 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+let element1 = document.getElementById("text");
+console.log(element1);
+
+
+let element2 = document.getElementsByTagName("h1");
+console.log(element2[0]);
+
+let element3 = document.getElementsByClassName("box");
+console.log(element3[0]);
+
+function changeText(){
+    let element4 = document.getElementsByTagName("h1");
+    element4[1].innerText = "Hello World"
+    console.log(element4[1]);
+}
+
+function changeDirection() {
+    let dummy = document.getElementsByClassName("flex-container");
+    dummy[0].style.flexDirection = "column";
+    dummy[0].style.alignItems = "flex-start";
+    console.log(dummy[0]);   
+}
+
+function changeColor(){
+    let element4 = document.getElementById("praveen");
+    element4.style.color = "blue";
+    element4.id = "heading";
+    console.log(element4);
+}
+
+function replaceText(){
+    let element5 = document.getElementsByTagName("p");
+    element5[0].innerText = "Welcome to Prepbytes";
+    element5[0].style.color = "red"
+    console.log(element5[0]);
+}
